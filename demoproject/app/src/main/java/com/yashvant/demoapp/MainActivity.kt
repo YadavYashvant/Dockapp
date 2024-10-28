16e18eccf11e88aa00c796501d48ff9f0f89cbe3
YadavYashvant <yashvantyadav855@gmail.com>
2024-10-28 23:22:16 +0530

    added a demo app

--- demoproject/app/src/main/java/com/yashvant/demoapp/MainActivity.kt
+++ demoproject/app/src/main/java/com/yashvant/demoapp/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     DemoappTheme {
-        Greeting("Android")
+        Greeting("Demo App")
     }
 }
